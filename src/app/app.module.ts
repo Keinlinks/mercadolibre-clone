dcf98229ec78d4df34cff72511256cf9bde26bb3
Keinlinks <kevin.clift.b@hotmail.com>
2023-07-19 06:46:09 -0400

    instalado ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HomeModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
