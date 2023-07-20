ef392e582117c5df363c48002aa713287f463e1f
Keinlinks <kevin.clift.b@hotmail.com>
2023-07-19 23:59:51 -0400

    ng-deep

--- src/app/home/components/slider-items/slider-items.component.ts
+++ src/app/home/components/slider-items/slider-items.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slider-items',
   templateUrl: './slider-items.component.html',
-  styleUrls: ['./slider-items.component.scss']
+  styleUrls: ['./slider-items.component.scss'],
 })
 export class SliderItemsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  images = [62, 83];
+  ngOnInit(): void {}
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
 }
